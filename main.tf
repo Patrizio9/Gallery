19dd3bbbee07419601ff65d62c141ce4ee8be28a
Patrizio9 <patriziogotti9@gmail.com>
2024-07-03 14:25:14 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,39 +12,18 @@ provider "docker" {
 
 resource "docker_image" "nginx_image" {
   name = "nginx:alpine"
-  build {
-    context    = abspath("${path.module}")
-    dockerfile = abspath("${path.module}/Dockerfile")
-  }
-}
 }
 
 resource "docker_container" "nginx_container" {
   image = docker_image.nginx_image.latest
-  name  = "web_server"
+  name  = "web-server"
   ports {
     internal = 80
-    external = 80
+    external = 8000
   }
-  # Monta la directory contenente i tuoi file nel container
+  # Monta la directory html nel container
   volumes {
-    host_path      = abspath("${path.module}/DC")
-    container_path = "/usr/share/nginx/Gallery/DC"
-  }
-  volumes {
-    host_path      = abspath("${path.module}/FantasySwords")
-    container_path = "/usr/share/nginx/Gallery/FantasySwords"
-  }
-  volumes {
-    host_path      = abspath("${path.module}/MISC")
-    container_path = "/usr/share/nginx/Gallery/Misc"
-  }
-  volumes {
-    host_path      = abspath("${path.module}/Sonic")
-    container_path = "/usr/share/nginx/Gallery/Sonic"
-  }
-  volumes {
-    host_path      = abspath("${path.module}/WorldOfHeroes")
-    container_path = "/usr/share/nginx/Gallery/WorldOfHeroes"
+    host_path      = abspath("${path.module}/All")
+    container_path = "/usr/share/nginx/html"
   }
 }
